10e8e05f64e253a89c5f54cdfd5796431dfaf9cd
JuanZLG <jczuluaga2003@gmail.com>
2023-08-27 01:18:53 -0500

    Setting Up

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Productos
 
 # Create your views here.
 
-# def home(request):
-#     return render(request, "prodHome.html")
+def home(request):
+    return render(request, 'baseInterface.html',{'productos': Productos.objects.all()})
