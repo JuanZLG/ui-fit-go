6664b1e6ad044e1c6ba0dccf30b3c3d896be445c
Jobserd12 <114456323+Jobserd12@users.noreply.github.com>
2023-08-29 20:28:23 -0500

    conexiondb en settings y configuracion raiz del sitio

--- tuiranfitgo/db.py
+++
@@ -1,9 +0,0 @@
-MYSQL = {
-    'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'tuiranfit',
-        'USER': 'root',
-        'PASSWORD': '3127123250',
-        'PORT': '3308'
-    }
-}
--- tuiranfitgo/urls.py
+++ tuiranfitgo/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin/', permanent=True)),
+    path('admin/', TemplateView.as_view(template_name='baseInterface.html'), name='base-interface'),
     path('admin/products', include('products.urls')),
     path('admin/eproviders', include('eproviders.urls')),  
 ]
