5d5f82b61bb636057440f7e5cbdb5cb1a9b96ae7
juan david <juanmartinezciro657@gmail.com>
2023-09-10 12:49:47 -0500

    idk

--- tuiranfitgo/settings.py
+++ tuiranfitgo/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'tuiranfitgo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.mysql',
-        'NAME':'tuiranfit',
+        'NAME':'_tuiranfit',
         'USER': 'root',
         'PASSWORD': '1234',
         'PORT': '3306'
