bde0626d854df0e724357f16a0fd946f402f199e
Camilo Zuluaga <jczuluaga2003@gmail.com>
2023-09-12 17:33:04 -0500

    No usen base de datos distinta

--- tuiranfitgo/settings.py
+++ tuiranfitgo/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'tuiranfitgo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.mysql',
-        'NAME':'_tuiranfit',
+        'NAME':'tuiranfit', #No trabajen con bases de datos distintas, yo les mando el script de la del sena.
         'USER': 'root',
-        'PASSWORD': '3127123250',
-        'PORT': '3306'
+        'PASSWORD': 'monitoc10',
+        'PORT': '1010'
     }
 }
 
