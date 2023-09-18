b05470e64f213207d984d8791c73bd9e30dc36f1
Camilo Zuluaga <jczuluaga2003@gmail.com>
2023-09-18 01:06:50 -0500

    Aveeeer eh.. EditProducts 15203 errores (o por ahi)

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include  # Funciones de manejo de rutas y urls
 from . import views 
 
-
 urlpatterns=[
     path('', views.Home, name='productos'), 
     path('addproduct/', views.createProduct, name="createAProduct"),
-    # path('Editar/', views.editar_proveedor, name='editarProveedor'),
+    path('editproduct/<int:producto_id>/', views.editProduct, name='editAProduct'),
     path('Estado/', views.cambiarEstadoDeProducto, name='cambiarEstadoDeProducto'),
 ]
--- tuiranfitgo/settings.py
+++ tuiranfitgo/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'tuiranfitgo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.mysql',
-        'NAME':'_tuiranfit',
+        'NAME':'tuiranfit',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'monitoc10',
         'PORT': '3306'
     }
 }
