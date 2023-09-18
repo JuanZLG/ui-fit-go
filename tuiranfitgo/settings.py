029e10f04d901ee7fd22cddb28a95d1a7813b003
Jobserd12 <114456323+Jobserd12@users.noreply.github.com>
2023-09-17 23:33:17 -0500

    Formateo precio

--- tuiranfitgo/settings.py
+++ tuiranfitgo/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'tuiranfitgo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.mysql',
-        'NAME':'_tuiranfit',
+        'NAME':'tuiranfit',
         'USER': 'root',
         'PASSWORD': '3127123250',
         'PORT': '3306'
