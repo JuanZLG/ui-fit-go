4abd0edbbadc9e03a2be4bdcc34495d7ae7f7018
Daniel <danielperezbedoya382@gmail.com>
2023-09-25 16:33:02 -0500

    cambios actuales

--- tuiranfitgo/settings.py
+++ tuiranfitgo/settings.py
@@ -115,7 +115,7 @@ LOGIN_REDIRECT_URL = '/mydashboard/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'users', 'static'),
     os.path.join(BASE_DIR, 'sales', 'static'),
--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from users.models import Roles, Permisos, Usuarios, Rolespermisos
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
 def login_view(request):
@@ -17,13 +17,16 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('dashboard')
+            return redirect('mydashboard')
         else:
             return render(request, 'login.html', {'error': 'Credenciales incorrectas'})
 
     return render(request, 'login.html')
 
 
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 
 def Home(request):
