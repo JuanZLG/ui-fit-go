d86a451fe320dd7fc709af3a61f76e492cfc14c3
Daniel <danielperezbedoya382@gmail.com>
2023-09-25 19:16:45 -0500

    login-animacion

--- tuiranfitgo/urls.py
+++ tuiranfitgo/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
+    path('admin/', TemplateView.as_view(template_name='login.html'), name='login'),
     path('admin/dashboard/', include('dashboard.urls')),
     path('admin/users/', include('users.urls')),
     path('admin/products/', include('products.urls')),
@@ -11,9 +15,8 @@ urlpatterns = [
     path('admin/clients/', include('customers.urls')),  
     path('admin/sales/', include('sales.urls')),  
     path('admin/purchases/', include('purchases.urls')),
-    path('', LoginView.as_view(template_name='login.html'), name='login'),
     path('login/', LoginView.as_view(template_name='login.html', success_url='/dashboard/'), name='login_view'),
-    path('mydashboard/', include('dashboard.urls')),
+    path('entrada/', include('dashboard.urls')),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
 
