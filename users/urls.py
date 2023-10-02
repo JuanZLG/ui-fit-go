515406091528675f70f3be294723915c80c79121
Camilo Zuluaga <jczuluaga2003@gmail.com>
2023-10-02 10:13:47 -0500

    Urls.py user

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,18 @@
-# from django.urls import path, include 
-# from . import views 
-# # from django.contrib.auth import views as auth_views
+from django.urls import path, include 
+from . import views 
+from django.contrib.auth import views as auth_views
 
-# urlpatterns=[
-#     path('', views.Home, name='usuarios'),
-#     path('adduser', views.createUser, name="createAUser"),
-#     path('edituser/<int:id_usuario>/', views.editUser, name='editAUser'),
-#     path('Estado/', views.cambiarEstadoDeUsuario, name='cambiarEstadoDeUsuario'),
-#     # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-#     # path('logout/', views.logout_view, name='logout'),
+urlpatterns=[
+    path('', views.Home, name='usuarios'),
+    path('adduser', views.createUser, name="createAUser"),
+    path('edituser/<int:id_usuario>/', views.editUser, name='editAUser'),
+    path('Estado/', views.cambiarEstadoDeUsuario, name='cambiarEstadoDeUsuario'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('Roles/', views.HomeRoles, name='HomeRoles'), 
+    path('action-rol/', views.accion_rol, name="accion_rol"),
+    path('obtener_datos/', views.obtener_datos, name='obtener_datos'),
+    path('rol_unico/', views.rol_unico, name='rol_unico'),
+    path('remove/', views.eliminar_rol, name='eliminarRol'),
+]
 
-    # path('Roles/', views.HomeRoles, name='HomeRoles'), 
-    # path('action-rol/', views.accion_rol, name="accion_rol"),
-    # path('obtener_datos_edicion/', views.obtener_datos_edicion, name='obtener_datos_edicion'),
-    # path('editRol', views.editRol, name="editar_rol"),
-    
-    
-# ]
