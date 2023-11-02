04e375cdf2325e7d727147f4b371ef947a5b3bfa
Jobserd12 <114456323+Jobserd12@users.noreply.github.com.>
2023-11-02 00:27:38 -0600

    ZzZzZzZzZzz

--- tuiranfitgo/settings.py
+++ tuiranfitgo/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'ENGINE':'django.db.backends.mysql',
         'NAME':'tuiranfit',
         'USER': 'root',
-        'PASSWORD': 'administrador',
+        'PASSWORD': '3127123250',
         'PORT': '3306'
     }
 }
