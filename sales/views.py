e9e24230215692b3a8aa40f06a20d7f208b15eab
Camilo Zuluaga <jczuluaga2003@gmail.com>
2023-11-12 22:21:06 -0500

    Antes de Pull Nov12

--- sales/views.py
+++ sales/views.py
@@ -7,14 +7,12 @@ from sales.models import Clientes, Detalleventa, Ventas, Productos, Marcas, Cate
 from django.db.models import Q
 # from .models import Notification
 
-@module_access_required('ventas')
-@jwt_cookie_required
+# @jwt_cookie_required
+# @module_access_required('ventas')
 def Home(request):
     ventas = Ventas.objects.all()
     return render(request, "salesHome.html", {"ventas": ventas})
 
-@module_access_required('ventas')
-@csrf_exempt
 @jwt_cookie_required
 def crear_venta(request):
     if request.method == 'POST':
